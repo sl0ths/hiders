1d742420ae1df9d5427382bee548693fc56d4442
penal <penal@penal.com>
2023-05-28 01:11:24 +0100

    stole file from guy at github

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+#[allow(dead_code)]
+fn print_matrix<T: std::fmt::Display>(vec: &[T]) {
+    for i in 0..8 {
+        for j in 0..8 {
+            print!("{:03} ", vec[i * 8 + j]);
+        }
+        print!("\n")
+    }
+}
+
+
 fn main() {
     println!("Hello, world!");
 }
