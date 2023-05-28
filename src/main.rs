ffd6f3b41bb91fb4b6a9c5d0ae0d13b9e3ddf6e9
AHMEDMERIMI <93890042+AHMEDMERIMI@users.noreply.github.com>
2023-05-28 01:38:20 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod transform;
 use crate::transform::*;
 use image::{DynamicImage, GenericImage, GenericImageView, Rgba};
+use std::env;
 use std::fs::File;
 use std::io::{BufReader, Lines};
+use std::path::Path;
 
 #[allow(dead_code)]
 fn print_matrix<T: std::fmt::Display>(vec: &[T]) {
@@ -12,6 +14,42 @@ fn print_matrix<T: std::fmt::Display>(vec: &[T]) {
         }
         print!("\n")
     }
+}
+
+fn find(image: &DynamicImage, vigor: usize, layer: usize) -> Vec<u8> {
+    let mut hidden_data = Vec::new();
+    let (width, height) = image.dimensions();
+    // Access individual pixels
+    for y in (0..height).step_by(8) {
+        let mut blocks = Vec::with_capacity(vigor);
+        for x in (0..width).step_by(8) {
+            let mut block: Vec<f32> = Vec::new();
+            let mut pixel;
+
+            for j in y..y + 8 {
+                for i in x..x + 8 {
+                    if i < height && j < width {
+                        pixel = image.get_pixel(i, j);
+                    } else {
+                        pixel = Rgba([0, 0, 0, 0]);
+                    }
+                    block.push(pixel[layer] as f32);
+                }
+            }
+
+            let dcteed_block = discrete_cosine_transform(&block);
+            for i in 64 - vigor..64 {
+                blocks.push(dcteed_block[i]);
+            }
+        }
+        hidden_data.push(blocks);
+    }
+    let hidden_data = hidden_data
+        .into_iter()
+        .flatten()
+        .map(|f| f.floor() as u8)
+        .collect::<Vec<u8>>();
+    hidden_data
 }
 
 #[allow(dead_code)]
@@ -81,5 +119,38 @@ fn hide(image: &mut DynamicImage, vigor: usize, lines: &mut Lines<BufReader<File
 }
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let usage = "usage: hiders [find vigor input_img | hide vigor input_img input_txt output_img]";
+    let action = args.get(1).expect(usage);
+    let vigor: usize = args.get(2).expect(usage).parse().unwrap();
+    let input = args.get(3).expect(usage);
+    let mut image = open(Path::new(input)).expect("Failed to open image");
+    let layer = 0;
+    // Get image dimensions
+    if action.eq_ignore_ascii_case("find") {
+        // finding in image
+        let hidden_data = find(&image, vigor, layer);
+        println!(
+            "hid data: {:?}\nlength = {}",
+            hidden_data,
+            hidden_data.len()
+        );
+    } else if action.eq_ignore_ascii_case("hide") {
+        // hiding in image
+        // hiders hide [text] [in image]
+        let data_to_hide = &args.get(4).expect(usage);
+        let output = &args.get(5).expect(usage);
+
+        // open the file for reading data to hide
+        let file = File::open(data_to_hide).unwrap();
+        let reader = BufReader::new(file);
+        let mut lines = reader.lines();
+
+        hide(&mut image, vigor, &mut lines, layer);
+        image.save(output).expect("failed to save");
+        println!("image saved")
+    } else {
+        println!("{usage}");
+        return;
+    }
 }
